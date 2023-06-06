69a9369b3ec94e9ac74cd6fc3298a1606c6706ef
gandradep <eventsonido@gmail.com>
2023-06-06 18:42:45 +0800

    Created triangl using Float32Array to send the vertices positions

--- src/script.js
+++ src/script.js
@@ -36,38 +36,33 @@ window.addEventListener('resize', () => {
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 })
 
-window.addEventListener('dblclick', () => {
-    const fullscreenElement  = document.fullscreenElement || document.webkitFullscreenElement
-    if (!fullscreenElement) {
-        if (canvas.requestFullscreen) {
-            canvas.requestFullscreen();
-        } else if (canvas.webkitRequestFullscreen) {
-            canvas.webkitFullscreen();
-        }
-    } else {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen();
-        }
-
-    }
-})
-
 // Scene
 const scene = new THREE.Scene()
 
 // Object
-const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
+// const geometry = new THREE.BufferGeometry();
+const geometry = new THREE.SphereGeometry(1)
+
+const count = 500;
+const positionsArray  = new Float32Array(count * 3 * 3);
+
+for(let i = 0; i< count * 3 * 3; i++){
+    positionsArray[i] = (Math.random() - 0.5) * 3;
+}
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+// geometry.setAttribute('position', positionsAttribute);
+
+const material = new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+    wireframe: true,
+   })
+const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
 camera.position.z = 3;
-camera.lookAt(mesh.position)
 scene.add(camera)
 
 //Controls
@@ -87,16 +82,8 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    // mesh.material.wireframe = !mesh.material.wireframe
 
-    // Update objects
-    // mesh.rotation.y = elapsedTime;
-
-    //Update camera
-    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-    // camera.position.y = -cursor.y * 3;
-    // camera.lookAt(mesh.position);
-    //Update controls
     controls.update();
 
     // Render
