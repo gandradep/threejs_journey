16a28c45d1f8fea22c2798b2dac857c8bd02273e
gandradep <eventsonido@gmail.com>
2023-06-21 14:46:34 +0800

    Adding text using typeface.json font

--- src/script.js
+++ src/script.js
@@ -1,39 +1,14 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 
 /**
  * Debug controls
  */
 const gui = new GUI();
 
-/**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader();
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-
-const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
-const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
-const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
-const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
-const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
-const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
-const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
-const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
-const gradientTexture = textureLoader.load('/textures/gradients/5.jpg')
-gradientTexture.minFilter = THREE.NearestFilter
-gradientTexture.magFilter = THREE.NearestFilter
-gradientTexture.generateMipmaps = false;
-
-const environmentMapTexture = cubeTextureLoader.load([
-    '/textures/environmentMaps/0/px.jpg',
-    '/textures/environmentMaps/0/nx.jpg',
-    '/textures/environmentMaps/0/py.jpg',
-    '/textures/environmentMaps/0/ny.jpg',
-    '/textures/environmentMaps/0/pz.jpg',
-    '/textures/environmentMaps/0/nz.jpg'
-])
 // Canvas
 
 const canvas = document.querySelector('canvas.webgl')
@@ -42,82 +17,64 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Objects
+ * Textures
  */
-// const material = new THREE.MeshBasicMaterial();
-// // material.wireframe = true
-// material.map = doorColorTexture
-// material.opacity = 0.5;
-// material.transparent = true;
-// // material.alphaMap = doorAlphaTexture;
-// material.side = THREE.DoubleSide
-
-// const material = new THREE.MeshNormalMaterial()
-// material.flatShading = true
-
-// const material = new THREE.MeshMatcapMaterial();
-// material.matcap = matcapTexture
-
-// const material = new THREE.MeshDepthMaterial()
-// material.side = THREE.DoubleSide
-
-// next materials need lights
-// const material = new THREE.MeshLambertMaterial();
-// const material = new THREE.MeshPhongMaterial();
-// material.shininess = 100;
-// material.specular = new THREE.Color(0xff00ff)
-
-// const material = new THREE.MeshToonMaterial();
-// material.gradientMap = gradientTexture
-
-// const material = new THREE.MeshStandardMaterial();
-// material.metalness = 0;
-// material.roughness = 1;
-// material.map = doorColorTexture
-// material.aoMap = doorAmbientOcclusionTexture
-// material.aoMapIntensity = 1
-// material.displacementMap = doorHeightTexture
-// material.displacementScale = 0.05
-// material.metalnessMap = doorMetalnessTexture
-// material.roughnessMap = doorRoughnessTexture
-// material.normalMap = doorNormalTexture
-// material.transparent = true;
-// material.alphaMap = doorAlphaTexture
-
-const material = new THREE.MeshStandardMaterial()
-material.metalness = 0.7
-material.roughness = 0.2
-material.envMap = environmentMapTexture
-
-gui.add(material, 'metalness').min(0).max(1).step(0.01)
-gui.add(material, 'roughness').min(0).max(1).step(0.01)
-gui.add(material, 'aoMapIntensity').min(0).max(10).step(0.02)
-gui.add(material, 'displacementScale').min(-2).max(-2).step(0.002)
+const textureLoader = new THREE.TextureLoader();
+const matcapTexture = textureLoader.load('/textures/matcaps/3.png')
 
 
-const sphere =  new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 160, 160),
-    material
-);
-sphere.position.x = -1.5;
+/**
+ * Fonts
+ */
+const fontLoader = new FontLoader();
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) => {
+        const textGeometry = new TextGeometry(
+            'Soon you will get it',
+            {
+                font,
+                size: 0.5,
+                height: 0.2,
+                curveSegments: 4,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 4
+            }
+        )
+        // textGeometry.computeBoundingBox()
+        // textGeometry.translate(
+        //     - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
+        //     - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
+        //     - (textGeometry.boundingBox.max.z - 0.03) * 0.5
+        // )
+        textGeometry.center();
 
-const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(1, 1, 100, 100),
-    material
+        const material = new THREE.MeshMatcapMaterial({matcap: matcapTexture})
+
+        const text = new THREE.Mesh( textGeometry, material)
+        scene.add(text);
+        console.time('donuts')
+        const donutGeometry = new THREE.TorusGeometry(0.3, 0.3, 20, 45)
+        for (let i = 0; i < 100; i++) {
+            const donut = new THREE.Mesh(donutGeometry, material)
+
+
+            donut.position.set((Math.random() - 0.5) * 10, (Math.random() - 0.5) * 10, (Math.random() - 0.5) * 10)
+
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
+
+            let scale = Math.random()
+            donut.scale.set(scale, scale, scale)
+
+            scene.add(donut);
+        }
+
+    }
 )
-// using ambienOcclusion we need uv array
-plane.geometry.setAttribute(
-    'uv2',
-    new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2)
-)
-
-const torus = new THREE.Mesh(
-    new THREE.TorusGeometry(0.5, 0.2, 160, 320),
-    material
-)
-torus.position.x = 1.5
-scene.add(plane, sphere, torus)
-
 /**
  * Lights
  */
@@ -140,17 +97,20 @@ window.addEventListener('resize', () => {
     sizes.width = window.innerWidth;
     sizes.height = window.innerHeight;
 
-    //update sizes
+    //update camera
     camera.aspect = sizes.width / sizes.height;
     camera.updateProjectionMatrix();
+
    // update renderer
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 })
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
 scene.add(camera)
 
 //Controls
@@ -169,17 +129,7 @@ const clock = new THREE.Clock()
 
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
-
     //update objects
-    sphere.rotation.y = elapsedTime * 0.1
-    plane.rotation.y = elapsedTime * 0.1
-    torus.rotation.y = elapsedTime * 0.1
-
-    sphere.rotation.x = elapsedTime * 0.15
-    plane.rotation.x = elapsedTime * 0.15
-    torus.rotation.x = elapsedTime * 0.15
-
     controls.update();
 
     // Render
