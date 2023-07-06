541376dc7d36e1815f66a8827a92bb023dfefec6
gandradep <eventsonido@gmail.com>
2023-07-06 17:16:22 +0800

    gsap to add rotation to object when reaching their section

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import * as dat from 'lil-gui'
+import gsap from "gsap";
 
 THREE.ColorManagement.enabled = false
 
@@ -145,10 +146,25 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Scroll
  */
 let scrollY = window.scrollY
-
+let currentSection = 0
 window.addEventListener('scroll', () =>
 {
   scrollY = window.scrollY
+
+  const newSection = Math.round(scrollY / sizes.height)
+
+  if (newSection !== currentSection) {
+    currentSection = newSection
+    gsap.to(
+      sectionMeshes[currentSection].rotation,
+      {
+        duration: 1.5,
+        ease: 'power2.inOut',
+        x: '+=6',
+        y: '+=3'
+      }
+    )
+  }
 
 })
 
@@ -186,8 +202,8 @@ const tick = () =>
 
     //update meshes
     for(const mesh of sectionMeshes){
-      mesh.rotation.x = elapsedTime * 0.05
-      mesh.rotation.y = elapsedTime * 0.04
+      mesh.rotation.x += deltaTime * 0.05
+      mesh.rotation.y += deltaTime * 0.04
     }
 
     // Render
