99f7bbfa0c225ac83e626eceaa43d49d4be20124
gandradep <eventsonido@gmail.com>
2023-07-17 14:13:04 +0800

    material qualities to plane and sphere so it bounces

--- src/script.js
+++ src/script.js
@@ -40,6 +40,19 @@ const environmentMapTexture = cubeTextureLoader.load([
 const world = new CANNON.World()
 world.gravity.set(0, -9.82, 0)
 
+//Materials
+const concreteMaterial = new CANNON.Material('concrete')
+const plasticMaterial = new CANNON.Material('plastic')
+
+const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+  concreteMaterial,
+  plasticMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.7
+  }
+)
+world.addContactMaterial(concretePlasticContactMaterial)
 
 
 // Sphere
@@ -47,13 +60,15 @@ const sphereShape = new CANNON.Sphere(0.5)
 const sphereBody = new CANNON.Body({
   mass: 1,
   position: new CANNON.Vec3(0, 3, 0),
-  shape: sphereShape
+  shape: sphereShape,
+  material: plasticMaterial
 })
 world.addBody(sphereBody)
 //floor
 const floorShape = new CANNON.Plane()
 const floorBody = new CANNON.Body()
 floorBody.mass = 0
+floorBody.material = concreteMaterial
 floorBody.addShape(floorShape)
 floorBody.quaternion.setFromAxisAngle(
   new CANNON.Vec3(-1, 0, 0),
