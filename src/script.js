88000d5b5f42d76e41008bf5e24a7a5874927f8d
gandradep <eventsonido@gmail.com>
2023-07-17 11:42:05 +0800

    Initialize project

--- src/script.js
+++ src/script.js
@@ -5,11 +5,13 @@ import GUI from 'lil-gui'
 THREE.ColorManagement.enabled = false
 
 /**
+ * Debug
+ */
+const gui = new GUI()
+
+/**
  * Base
  */
-// Debug
-// const gui = new GUI()
-
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -20,51 +22,66 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
-const particleTexture = textureLoader.load('/textures/particles/8.png')
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+const environmentMapTexture = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.png',
+    '/textures/environmentMaps/0/nx.png',
+    '/textures/environmentMaps/0/py.png',
+    '/textures/environmentMaps/0/ny.png',
+    '/textures/environmentMaps/0/pz.png',
+    '/textures/environmentMaps/0/nz.png'
+])
 
 /**
- * Particles
+ * Test sphere
  */
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    new THREE.MeshStandardMaterial({
+        metalness: 0.3,
+        roughness: 0.4,
+        envMap: environmentMapTexture,
+        envMapIntensity: 0.5
+    })
+)
+sphere.castShadow = true
+sphere.position.y = 0.5
+scene.add(sphere)
 
-// const particleGeometry = new THREE.SphereGeometry(1, 32, 32)
-const particleGeometry = new THREE.BufferGeometry()
-const count = 5000
+/**
+ * Floor
+ */
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshStandardMaterial({
+        color: '#777777',
+        metalness: 0.3,
+        roughness: 0.4,
+        envMap: environmentMapTexture,
+        envMapIntensity: 0.5
+    })
+)
+floor.receiveShadow = true
+floor.rotation.x = - Math.PI * 0.5
+scene.add(floor)
 
-const positions = new Float32Array(count * 3)
-const colors = new Float32Array(count * 3)
-for(let i = 0; i<count * 3; i++){
-    positions[i] = (Math.random() - 0.5) * 10
-    colors[i] = Math.random()
-}
-particleGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
-particleGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.7)
+scene.add(ambientLight)
 
-const particleMaterial = new THREE.PointsMaterial({
-    size: 0.1,
-    sizeAttenuation: true, //create perspective from camera
-    color: '#ffff00'
-})
-
-particleMaterial.transparent = true
-particleMaterial.alphaMap = particleTexture
-//alphaTest: material wont be rendered if the opacity is lower than this value.
-// particleMaterial.alphaTest = 0.001
-// particleMaterial.depthTest = false
-particleMaterial.depthWrite = false
-particleMaterial.blending = THREE.AdditiveBlending
-//vertex coloring is used???
-particleMaterial.vertexColors = true
-
-const particles = new THREE.Points(particleGeometry, particleMaterial)
-scene.add(particles)
-
-//cube
-// const cube = new THREE.Mesh(
-//     new THREE.BoxGeometry(),
-//     new THREE.MeshBasicMaterial()
-// )
-// scene.add(cube)
-
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
 
 /**
  * Sizes
@@ -94,7 +111,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 10
+camera.position.set(- 3, 3, 3)
 scene.add(camera)
 
 // Controls
@@ -108,6 +125,8 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.outputColorSpace = THREE.LinearSRGBColorSpace
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
@@ -120,15 +139,6 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
-    //Update particles
-    // particles.rotation.y = elapsedTime * 0.1
-    // not the best solution to animate
-    for(let i = 0; i < count; i++){
-        const i3 = i * 3
-        const x = particleGeometry.attributes.position.array[i3]
-        particleGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x) *2
-    }
-    particleGeometry.attributes.position.needsUpdate = true
     // Update controls
     controls.update()
 
