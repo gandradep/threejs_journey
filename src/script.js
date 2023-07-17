db7c4cd87f02ce89d21410e62c0498a474240be1
gandradep <eventsonido@gmail.com>
2023-07-17 11:52:29 +0800

    Adding Cannon physics library

--- src/script.js
+++ src/script.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import CANNON from 'cannon'
+
+console.log(CANNON);
 
 THREE.ColorManagement.enabled = false
 
