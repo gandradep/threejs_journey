48586adaa153daa0eb065ab551ec7fa50e7d8e0e
gandradep <eventsonido@gmail.com>
2023-07-17 14:19:36 +0800

    same default material to elements

--- src/script.js
+++ src/script.js
@@ -41,19 +41,18 @@ const world = new CANNON.World()
 world.gravity.set(0, -9.82, 0)
 
 //Materials
-const concreteMaterial = new CANNON.Material('concrete')
-const plasticMaterial = new CANNON.Material('plastic')
+const defaultMaterial = new CANNON.Material('default')
 
-const concretePlasticContactMaterial = new CANNON.ContactMaterial(
-  concreteMaterial,
-  plasticMaterial,
+const defaultContactMaterial = new CANNON.ContactMaterial(
+  defaultMaterial,
+  defaultMaterial,
   {
     friction: 0.1,
     restitution: 0.7
   }
 )
-world.addContactMaterial(concretePlasticContactMaterial)
-
+world.addContactMaterial(defaultContactMaterial)
+world.defaultContactMaterial = defaultContactMaterial
 
 // Sphere
 const sphereShape = new CANNON.Sphere(0.5)
@@ -61,14 +60,12 @@ const sphereBody = new CANNON.Body({
   mass: 1,
   position: new CANNON.Vec3(0, 3, 0),
   shape: sphereShape,
-  material: plasticMaterial
 })
 world.addBody(sphereBody)
 //floor
 const floorShape = new CANNON.Plane()
 const floorBody = new CANNON.Body()
 floorBody.mass = 0
-floorBody.material = concreteMaterial
 floorBody.addShape(floorShape)
 floorBody.quaternion.setFromAxisAngle(
   new CANNON.Vec3(-1, 0, 0),
